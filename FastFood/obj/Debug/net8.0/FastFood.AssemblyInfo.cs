b84007fd43f93bee2d5c0ef5fc4b89e513c72893
mali <moohamedali571@gmail.com>
2024-10-12 06:30:05 +0300

    Add StartProcess,Ship And Cancel Actions in order controller

--- FastFood/obj/Debug/net8.0/FastFood.AssemblyInfo.cs
+++ FastFood/obj/Debug/net8.0/FastFood.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FastFood")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdb8696910403bd8aecad49f69cb00ab2589e0d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e6319100f4d00ca6f574c72fe0502f11b9affff")]
 [assembly: System.Reflection.AssemblyProductAttribute("FastFood")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FastFood")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
